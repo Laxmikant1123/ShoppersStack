998bdb38233f24413b6d60f83c20847c7937f052
laxmi <laxmi@HP>
2023-06-07 11:23:30 +0530

    1st Commit

---
+++ ShoppersStack/src/test/java/com/ss/testscripts/CustomerModule.java
@@ -0,0 +1,13 @@
+package com.ss.testscripts;
+
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class CustomerModule {
+
+	
+	@Test
+	void createCust() {
+		Reporter.log("Create Customer",true);
+	}
+}
